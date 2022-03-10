e0493c41e97f770fbb242b24a24b4ab85c02bfe0
Insane hvh <1751707@etu.cegep-lanaudiere.qc.ca>
2022-03-10 13:12:42 -0500

    ajout atelier 1.4

---
+++ atelier1.4/EtreVivant.php
@@ -0,0 +1,6 @@
+<?php
+
+class EtreVivant
+{
+    public int $vieMax = 10, $vieActu, $force = 5, $defense = 5;
+}
---
+++ atelier1.4/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<h1>Atelier 1.4</h1>
+
+<div class="form-check">
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+    <label class="form-check-label" for="flexRadioDefault1">
+        Guerrier
+    </label>
+</div>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+    <label class="form-check-label" for="flexRadioDefault2">
+        Assassin
+    </label>
+</div>
